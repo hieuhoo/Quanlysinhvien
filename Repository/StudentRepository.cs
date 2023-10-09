89acb686332226fe765590ddb26dfac50e5221c4
hieuhoo <115386303+hieuhoo@users.noreply.github.com>
2023-10-09 09:21:20 +0700

    Update StudentRepository.cs

--- Repository/StudentRepository.cs
+++ Repository/StudentRepository.cs
@@ -1,4 +1,5 @@
-﻿using NHibernate;
+using FluentNHibernate.Visitors;
+using NHibernate;
 using NHibernate.Linq;
 using Quanlysinhvien;
 using Quanlysinhvien.Models;
@@ -23,11 +24,43 @@ namespace Quanlysinhvien
 
         public void Addnewstudent(sinhvien student)
         {
+            using (var session =_session.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        session.Save(student);
+                        transaction.Commit();
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine($"Error message :{ex.Message}");
+                        transaction.Rollback();
+                    }
+                }    
+            }    
             throw new NotImplementedException();
         }
 
         public void Deletestudent(sinhvien student)
         {
+            using (var session = _session.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        session.Delete(student);
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error message :{ex.Message}");
+                        transaction.Rollback();
+                    }
+                }
+            }
             throw new NotImplementedException();
         }
 
@@ -43,14 +76,32 @@ namespace Quanlysinhvien
 
         public List<sinhvien> Sortbyname()
         {
+          
             throw new NotImplementedException();
         }
 
         public void Updatenewstudent(sinhvien studentUpdate)
         {
+            using (var session = _session.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        session.Update(studentUpdate);
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error message :{ex.Message}");
+                        transaction.Rollback();
+                    }
+                }
+            }
             throw new NotImplementedException();
         }
     }
 
     
 }
+
